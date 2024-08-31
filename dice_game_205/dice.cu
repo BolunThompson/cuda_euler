4dbb26d9e62881b6729b1032aeffae789ad4b931
Bolun Thompson <me@bolun.dev>
2024-08-31 19:55:31 +0000

    add half-working faulty 4+2 solution

--- dice_game_205/dice.cu
+++ dice_game_205/dice.cu
@@ -17,79 +17,69 @@
     }                                                                          \
   } while (0)
 
-#define REPLACE_TOO_BIG_RAND(rand_var, test, rep_bits)                         \
-  do {                                                                         \
-    if ((rand_var & (test)) == (test)) {                                       \
-      std::printf(                                                             \
-          "inputs:\nrand_var: %0#010x\ntest: %0#010x\nrep_bits: %0#010x\n\n",  \
-          rand_var, test, rep_bits);                                           \
-      while ((rep_rand & 0x6) == 0x6)                                          \
-        rep_rand >>= 3;                                                        \
-      if (rep_rand == 0xc)                                                     \
-        rep_rand = curand(&state) | 0xc0000000;                                \
-      rand_var = (rand_var & ~(rep_bits)) | (rep_rand & 0x7);                  \
-      rep_rand >>= 3;                                                          \
-      std::printf("outputs:\nrand_var: %0#010x\ntest: %0#010x\nrep_bits: "     \
-                  "%0#010x\n\n\n",                                             \
-                  rand_var, test, rep_bits);                                   \
-    }                                                                          \
-  } while (0)
+constexpr int BLOCKS = 1;
+constexpr int THREADS = 1;
+constexpr int SEED = 0xFAB39;
+constexpr unsigned long long ITERS = 1e3;
+// won't exactly lead to ITERS iterations, but close enough.
+constexpr unsigned long long PER_THREAD = ITERS / (BLOCKS * THREADS * 3);
+constexpr unsigned long long ACTUAL_ITERS = PER_THREAD * (BLOCKS * THREADS * 3);
 
-__global__ void game(unsigned int const per_thread, unsigned int const seed,
-                     unsigned long long *const d_out) {
+__device__ __forceinline__ unsigned int
+add4sum(unsigned int v1, unsigned int v2, unsigned int initial) {
+  return __dp4a(__vadd4(v1, v2), 0x01010101U, initial);
+}
+
+__device__ __forceinline__ unsigned int peter_game(unsigned int const rand) {
+  // TODO: Would using a tensor core speed up this operation?
+  // extracts bits 0 and 1 from each byte
+  unsigned int temp1 = rand & 0x03030303;
+  // extract bits 2 and 3 from each byte
+  unsigned int temp2 = (rand & 0x0c0c0c0c) >> 2;
+  // the initial value is bit 6 and 7 from the third byte
+  return add4sum(temp1, temp2, ((rand & 0xc00000) >> 22) + 9);
+}
+
+__global__ void game(unsigned long long *const d_out) {
   const int bt_ind = blockDim.x * blockIdx.x + threadIdx.x;
-  if (bt_ind == 0)
-    *d_out = 0;
 
   // each thread needs its own state
   curandState state;
-  curand_init(seed, bt_ind, 0, &state);
+  curand_init(SEED, bt_ind, 0, &state);
 
   unsigned int outcome = 0;
-  for (int i = 0; i < per_thread; ++i) {
-    // TODO: Replace repeats with preprocessor macros
+  // First loop, so I generate random colin numbers from 1-4
+  // #pragma unroll
+  for (int i = 0; i < PER_THREAD * 2; ++i) {
     unsigned int rand = curand(&state);
-    unsigned int temp1 =
-        rand & 0x03030303; // extracts bits 0 and 1 from each byte
-    unsigned int temp2 =
-        (rand & 0x0c0c0c0c) >> 2; // extract bits 2 and 3 from each byte
-    // the initial value is bit 4 and 5 from the first byte
-    // TODO: Would using a tensor core speed up this operation?
-    auto peter_res =
-        __dp4a(__vadd4(temp1, temp2), 0x01010101U, (rand & 0x30) >> 4) + 9;
-    // At this point, I've only used 18 bits of the 32 bits of randomness
-    // provided If I simply generate another 32 bits and discard values of 6 or
-    // 7, I'd on average be able to generate 11 more numbers. I need 6. However,
-    // there's an uncomfortably high chance of [] that I don't get all the
-    // values I need. In that case, the chance of 6 or more of those 11 numbers
-    // being a 6 or 7 is 3.43% (calculation done via the binomial theorem).
-    // Therefore, for some threads, another random number will be generated.
+    unsigned int peter_res = peter_game(rand);
+    // At this point, I've used 18 bits of the 32 bits of randomness.
 
-    // NOTE: The actual chances above are higher than calculated since I'm
-    // throwing away the highest bit of randomness in each byte (and the
-    // remaining) two in the first. I don't know if it's faster to extract those
-    // or to generate somewhat more random numbers (TODO profile).
-
-    // When one value is above the range (6 or 7), that value is replaced with
-    // the next 3 valid bits in rep_rand. When replacements == sentinel, all
-    // the values have been exhausted (will happen in 7.81% of runs) a new
-    // replacements will be generated.
-    rand = curand(&state);
-    unsigned int rep_rand = curand(&state) | 0xc0000000;
-    // extract the first 3 bits of each byte
-    temp1 = rand & 0x07070707;
-    // extract the second 3 bits of the last two bytes
-    temp2 = (rand & 0x00003838) >> 3;
-    // if bits 1 and 2 in the first byte are set (meaning that the value
-    // equals 6 or 7), replace bits 0-2 with the next 3 bits in rep_rand.
-    REPLACE_TOO_BIG_RAND(temp1, 0x6, 0x7);
-    REPLACE_TOO_BIG_RAND(temp1, 0x6 << 8, 0x7 << 8);
-    REPLACE_TOO_BIG_RAND(temp1, 0x6 << 16, 0x7 << 16);
-    REPLACE_TOO_BIG_RAND(temp1, 0x6 << 24, 0x7 << 24);
-    REPLACE_TOO_BIG_RAND(temp2, 0x6, 0x7);
-    REPLACE_TOO_BIG_RAND(temp2, 0x6 << 8, 0x7 << 8);
-    auto colin_res = __dp4a(__vadd4(temp1, temp2), 0x01010101U, 6U);
+    // extract bits 4 and 5 from each byte
+    unsigned int temp1 = (rand & 0x30303030) >> 4;
+    // extract bits 6 and 7 from the first two bytes
+    unsigned int temp2 = (rand & 0x0000c0c0) >> 6;
+    // initial value is 6 to account for a die being from 1-6
+    unsigned int colin_res = add4sum(temp1, temp2, 6);
     outcome += (peter_res > colin_res);
+    // I've used 30 out of 32 bits of randomness
+  }
+  // Likewise, but I generate random colin numbers from 5-6
+  // #pragma unroll
+  // TODO: is there a more compact way to do these calculations?
+  for (int i = 0; i < PER_THREAD; ++i) {
+    unsigned int rand = curand(&state);
+    unsigned int peter_res = peter_game(rand);
+    // Extract bit 4 from each byte
+    unsigned int temp1 = (rand & 0x10101010) >> 4;
+    // Extract bit 6 from the first two bytes
+    unsigned int temp2 = (rand & 0x4040) >> 6;
+    // initival value is 14 because 6 + 2 * 4, since the dice are from 5-6
+    unsigned int colin_res = add4sum(temp1, temp2, 14);
+    std::printf("temp1: %#010x, temp2: %#010x\n", temp1, temp2);
+    std::printf("peter: %u, colin: %u\n\n", peter_res, colin_res);
+    outcome += (peter_res > colin_res);
+    // I've used 24 bits of randomness
   }
 
   // I don't think using shared memory would boost
@@ -103,27 +93,26 @@ __global__ void game(unsigned int const per_thread, unsigned int const seed,
 int main(int argc, char **argv) {
   // TODO: Switch blocks/threads count to optimal. See CUDA
   // programming guide.
-  constexpr int blocks = 1;
-  constexpr int threads = 1;
-  constexpr unsigned long iters = 1e3;
-  // won't exactly lead to iters iterations, but close
-  // enough.
-  constexpr unsigned int per_thread = iters / (blocks * threads);
 
+  auto h_out = new unsigned long long;
   unsigned long long *d_out;
   cudaMalloc(&d_out, sizeof(unsigned long long));
+  checkCudaErrors();
+  cudaMemset(d_out, 0, sizeof(unsigned long long));
   checkCudaErrors();
 
   std::printf("game run!\n");
 
-  game<<<blocks, threads>>>(per_thread, 0xFAB39, d_out);
+  game<<<BLOCKS, THREADS>>>(d_out);
   cudaDeviceSynchronize();
   checkCudaErrors();
 
-  auto h_out = new unsigned long long;
   cudaMemcpy(h_out, d_out, sizeof(unsigned long long), cudaMemcpyDeviceToHost);
-  // TODO: would this work with float64?
-  double result = static_cast<__float128>(*h_out) / iters;
+  // In principle, if h_out > 2^53 (9e15), a double could start creating
+  // smal calculation errors. A long double gives us exact precision up
+  // to 2^63 (9e18), which is enough.
+  // Not using ITERS directly because PER_THREAD rounded it down
+  double result = static_cast<long double>(*h_out) / ACTUAL_ITERS;
 
   std::printf("ANSWER: %.8f\n", result);
 
